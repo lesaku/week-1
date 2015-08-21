ec11dbdf5906311af22a6cbe26a4622d9b27f692
Jiraphat <lesaku--like@hotmail.com>
2015-08-21 17:50:41 +0700

    without flag

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -16,9 +16,7 @@ namespace Lab1_1
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
 
             // process
-            while (flag)
-            {
-                flag = false;
+            
                 for (int i = 0; i != input.Length - 1; i++)
                 {
                     if (input[i] < input[i + 1])
@@ -29,7 +27,7 @@ namespace Lab1_1
                         flag = true;
                     }
                 }
-            }
+            
 
             // output
             for (int i = 0; i != input.Length; i++)
