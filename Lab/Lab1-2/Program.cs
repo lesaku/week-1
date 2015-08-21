ba9bafb495b4b43d7cadf76309abf287a326f7bd
Jiraphat <lesaku--like@hotmail.com>
2015-08-21 17:57:23 +0700

    three function

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -14,8 +14,13 @@ namespace Lab1_2
 
             // input
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            // process
+            process(input);
+            output(input);
+            Console.ReadKey();
+        }
+        static void process(int[] input)
+        {
+            bool flag = true;
             while (flag)
             {
                 flag = false;
@@ -30,14 +35,14 @@ namespace Lab1_2
                     }
                 }
             }
-
-            // output
+        }
+        static void output(int[] input)
+        {
             for (int i = 0; i != input.Length; i++)
             {
                 Console.Write(input[i]);
                 Console.Write(" ");
             }
-            Console.ReadKey();
         }
     }
 }
